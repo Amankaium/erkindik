c3f241e2edd5e3595404f6e1203722c484ebb6d3
bibinurgazieva <tulipus11@gmail.com>
2022-05-27 12:55:49 +0600

    added styles to artinfo

--- src/pages/ArtInfo/ArtInfo.js
+++ src/pages/ArtInfo/ArtInfo.js
@@ -8,30 +8,30 @@ export default function ArtInfo () {
         <div className="page">
             <div className="author-header">
                 <div className="user-box">
-                    <div className="user-name">Artist</div>
-                    <div className="artist-name">Bibinur Nurgazieva</div>
+                    <div className="user-name">Автор</div>
+                    <div className="artist-name">Гапар Айтиев</div>
                 </div>
             </div>
             <div className="content">
                 <div className="picture-cont">
                     <div className="picture">
-                        <img height="350px" width="450px" src={img1} />
+                        <img height="350px" width="450px" src={img1} alt="sarychelek" />
                     </div>
                 </div>
                 <div className="info-cont">
-                    <h1 className="paint-name">Naturmort</h1>
-                    <h3 className="author-name">Bibinur</h3>
+                    <h1 className="paint-name">Сары-Челек</h1>
+                    <h3 className="author-name">Гапар Айтиев</h3>
                 <hr className="info-hr"></hr>
                     <div className="info-list">
-                        <div className="year">Year: 2022</div> 
-                        <div className="material">Material: Paper, print</div>
-                        <div className="status">Status: For sale</div>
-                        <div className="dimension">Dimensions: 350x450 px</div>
+                        <div className="year">Год: 2022</div> 
+                        <div className="material">Материал: Бумага, принт</div>
+                        <div className="status">Статус: На продажу</div>
+                        <div className="dimension">Размеры: 350x450 px</div>
                     </div>
                 <hr className="info-hr"></hr>
-                    <div className="price">Price: 3200</div>
+                    <div className="price">Цена: 3200</div>
                 <hr className="info-hr"></hr>
-                    <button className="buy-now">Buy Now</button>
+                    <button className="buy-now">Купить</button>
                     <div className="instruct-text">
                         <p>Нажимая на кнопку Купить, вы переходите на профиль пользователя, где можете связаться с ним в наиболее удобной форме</p>
                     </div>
@@ -39,16 +39,16 @@ export default function ArtInfo () {
                
             </div>
             <div className="otherworks">
-                    <div className="other-work-text">Other works of the author</div>
+                    <div className="other-work-text">Остальные работы автора</div>
                     <div className="works-box">
                         <div className="grid-box">
-                        <img height="200px" width="250px" src={img1} /> 
+                        <img height="200px" width="250px" src={img1} alt="sarychelek"/> 
                         </div>
                         <div className="grid-box">
-                        <img height="200px" width="250px" src={img1} />
+                        <img height="200px" width="250px" src={img1} alt="sarychelek" />
                         </div>
                         <div className="grid-box">
-                        <img height="200px" width="250px" src={img1} />
+                        <img height="200px" width="250px" src={img1} alt="sarychelek" />
                         </div>
                     </div>
                 </div>
