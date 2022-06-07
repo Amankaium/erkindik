0642ab9e18f67d7d5dc37fe1d34e818a4d35bb4d
Suiunduk <32750115+Suiunduk@users.noreply.github.com>
2022-06-07 14:13:52 +0600

    About us page added

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import SignIn from './SignIn/SignIn';
 import SignUp from './SignUp/SignUp';
 import Search from './Search/Search';
 import TermsCon from './TermsAndCondition/TermsCon';
+import AboutUs from './AboutUs/AboutUs';
 
 
-export {Artists, Homepage, SignIn, SignUp, Search, TermsCon};
+export {Artists, Homepage, SignIn, SignUp, Search, TermsCon, AboutUs};
